497665996a9355df40468f109595fe407b1f576e
Samecoins <146867889+Samecoins@users.noreply.github.com>
2024-04-18 12:45:19 +0400

    Выполнение

---
+++ src/LabWork02/HuffmanNode.java
@@ -0,0 +1,9 @@
+package LabWork02;
+
+public class HuffmanNode {
+    int data;
+    char c;
+
+    HuffmanNode left;
+    HuffmanNode right;
+}
---
+++ src/LabWork02/Main.java
@@ -0,0 +1,7 @@
+package LabWork02;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
